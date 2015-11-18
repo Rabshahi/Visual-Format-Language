5fda2ca7d75a011240806db3f635cf64cdf07da0
Rowin Shahi <Rowin@rowins-macbook-pro.local>
2015-11-19 00:42:21 +0545

    Initial Commit

---
+++ TableViewPro/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TableViewPro
+//
+//  Created by Rowin Shahi on 11/19/15.
+//  Copyright © 2015 Rowin Shahi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ TableViewPro/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TableViewPro
+//
+//  Created by Rowin Shahi on 11/19/15.
+//  Copyright © 2015 Rowin Shahi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
